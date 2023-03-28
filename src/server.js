b10db850629bfea9fdb31fdbd5ba1dcff71098bf
勝野継太 <katsuno.keita@gmail.com>
2023-03-28 11:17:31 +0900

    first commit

--- src/server.js
+++ src/server.js
@@ -14,23 +14,35 @@ const setUpServer = () => {
     } else {
       result = await phoneModel.getOne(id);
     }
+    let resCode;
+    let message;
     let response = [];
-    response.push({ responseCode: "0000", message: "Get complete" });
+    if (result.length === 0) {
+      resCode = "0001";
+      message = "Not Applicable (N/A)";
+    } else {
+      resCode = "0000";
+      message = "Get complete";
+    }
+    response.push({ responseCode: resCode, message: message });
     response.push(result);
     res.type("json").send(response);
   });
 
   app.post("/phones", async (req, res) => {
     const body = req.body;
-    //console.log(body);
-    const result = await phoneModel.addPhone(body);
-    //console.log(result);
-    const addData = await phoneModel.getOne(result);
-    console.log(addData);
-    let response = [];
-    response.push({ responseCode: "0000", message: "Add complete" });
-    response.push(addData);
-    res.status(201).send(response);
+    if (body.phone_name) {
+      const result = await phoneModel.addPhone(body);
+      const addData = await phoneModel.getOne(result);
+      let response = [];
+      response.push({ responseCode: "0000", message: "Add complete" });
+      response.push(addData);
+      res.status(201).send(response);
+    } else {
+      let response = [];
+      response.push({ responseCode: "0001", message: "Request error" });
+      res.status(400).send(response);
+    }
   });
 
   app.patch("/phones", async (req, res) => {
@@ -68,13 +80,31 @@ const setUpServer = () => {
   });
 
   app.delete("/phones", async (req, res) => {
-    const body = req.body;
-    const result = await phoneModel.deletePhone(body);
+    const query = req.query;
     const allData = await phoneModel.getAll();
+    let index = 0;
+    for (let key in allData) {
+      //console.log(allData[key].id, query.id);
+      if (allData[key].id === Number(query.id)) {
+        await phoneModel.deletePhone(query);
+        index = 1;
+      }
+    }
+    let resCode;
+    let message;
     let response = [];
-    response.push({ responseCode: "0000", message: "Delete complete" });
-    response.push(allData);
-    res.send(response);
+    if (index === 0) {
+      resCode = "0001";
+      message = "Not Applicable (N/A)";
+      response.push({ responseCode: resCode, message: message });
+    } else if (index === 1) {
+      resCode = "0000";
+      message = "Delete complete";
+      const result = await phoneModel.getAll();
+      response.push({ responseCode: resCode, message: message });
+      response.push(result);
+    }
+    res.type("json").send(response);
   });
 
   return app;
